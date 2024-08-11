03fac68a3070e8dc1d2f76b76c46b954fd693af0
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2024-08-11 16:38:39 +0500

    Completed + learned useCallack, useEffect, useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 const App = () => {
-  const [length, setlength] = useState();
+  const [length, setlength] = useState(0);
   const [numAllowed, setNumAllowed] = useState(false);
   const [CharAllowed, setCharAllowed] = useState(false);
   const [pass, setPass] = useState("");
+
+  const passwordRef = useRef(null);
+
+  const copyPass = useCallback(()=>{
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(pass)
+  }, [pass])
 
   const passGen = useCallback(() => {
     let password = "";
@@ -13,25 +20,30 @@ const App = () => {
     if (numAllowed) str += "012356789";
     if (CharAllowed) str += "!@#$%^&*{}[]?/;:<>";
 
-    for (let i = 0 ; i <= length; i++) {
-      const randomPass = Math.floor(Math.random() * str.length + 1);
+    for (let i = 1; i <= length; i++) {
+      let randomPass = Math.floor(Math.random() * str.length + 1);
       password += str.charAt(randomPass);
     }
 
     setPass(password);
-  }, [numAllowed, CharAllowed, length]);
-  
+  }, [numAllowed, CharAllowed, length, setPass]);
+
   useEffect(() => {
     passGen();
   }, [numAllowed, CharAllowed, length, passGen]);
-  console.log(pass)
 
   return (
     <div className="bg-stone-600 mx-auto rounded-md w-[80%] p-6 my-6 flex flex-col">
       <h1 className="text-center text-3xl">Password Generator</h1>
       <div className="w-full border flex justify-between my-4 mx-auto">
-        <input type="text" className="w-[80%]" value={pass} readOnly/>
-        <button className="text-center text-xl bg-blue-600 w-[20%]">
+        <input
+          type="text"
+          className="w-[80%] text-black"
+          value={pass}
+          ref={passwordRef}
+          readOnly
+        />
+        <button className="text-center text-xl bg-blue-600 w-[20%]" onClick={copyPass}>
           COPY
         </button>
       </div>
@@ -44,14 +56,22 @@ const App = () => {
           onChange={(e) => setlength(e.target.value)}
         />
         <label htmlFor="password">Length({length})</label>
-        <input type="checkbox" id="num" onChange={(prev) => !prev} defaultChecked={numAllowed}/>
-        <label htmlFor="num" >
-          Numbers
-        </label>
-        <input type="checkbox" id="char" onChange={(prev) => !prev} defaultChecked={CharAllowed}/>
-        <label htmlFor="char" >
-          Characters
-        </label>
+        <input
+          type="checkbox"
+          id="num"
+          onClick={() => setNumAllowed((prev) => !prev)}
+          defaultChecked={numAllowed}
+        />
+        <label htmlFor="num">Numbers</label>
+        <input
+          type="checkbox"
+          id="char"
+          onClick={() => {
+            setCharAllowed((prev) => !prev);
+          }}
+          defaultChecked={CharAllowed}
+        />
+        <label htmlFor="char">Characters</label>
       </div>
     </div>
   );
